4e0d171cb1fc7cb7bd141bfe16eaef45e082a4f2
SungJimWoo <134564164+SungJimWoo@users.noreply.github.com>
2025-06-20 21:37:14 +0000

    Pastas novas

---
+++ Python/basico/poo_intro.py
@@ -0,0 +1,16 @@
+
+class Player:
+    host = "localhost:8080" # Global
+    # inicializador do objeto
+    # passando valores posicionais
+    def __init__(self, nome, arma):
+        self.nome = nome # autoriza modificação/objeto
+        self.arma = arma 
+
+
+kratos: Player = Player("kratos", "Lâminas do caos")
+
+Hades: Player = Player("Hades", "Stygius")
+print(kratos.nome, kratos.arma)
+print(Hades.nome, Hades.host)
+
