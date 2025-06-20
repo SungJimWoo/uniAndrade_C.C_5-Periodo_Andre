e207e66747b4ab62dd8d16ca2f0042132b2b5a98
SungJimWoo <134564164+SungJimWoo@users.noreply.github.com>
2025-06-20 22:11:55 +0000

    Atualizado

--- Python/basico/types.py
+++ Python/basico/types.py
@@ -1,28 +1,31 @@
-
 class A:
-    ...#ellipsis
-class B:
-    ...#ellipsis
-class C(A, B):
-    ...#ellipsis
+    pass
 
-#Type anotations
+class B:
+    pass
+
+class C(A, B):
+    pass
+
+# Type annotations
 idade: int = 32
-salario: float + 35000.50
+salario: float = 35000.50
 nome: str = "Julius"
 casado: bool = True
+
 dados: dict = {"nome": nome, "salario": salario, "idade": idade}
-array: list = [2,5, "ju", 25, 25, 25]
-tupla: tuple = (2,5, 25, 25)
-unico: set = {2,5,"ju", 25, 25, 25 }
+array: list = [2.5, "ju", 25, 25, 25]
+tupla: tuple = (2.5, 25, 25, 25)
+unico: set = {2.5, "ju", 25, 25, 25}
 
 # print(unico)
-print(nome.upper()) # metodo builtin
-print (vars(C))     # imprime class com dict
-#help(C)            # ajuda ver internamente
+print(nome.upper())  # método builtin
+print(vars())        # imprime class com dict
+# help(C)            # ajuda ver internamente
+
 nome: str = "Ana Paula"
 eh_casado: bool = True
-pessoa: A = A() #Tipo personalizado
+pessoa: A = A()      # tipo personalizado
 A.cargo = "diretor"
 
-print(A._dict_) # aramazena valores da class
+# print(A.__dict__)  # armazena valores da class
